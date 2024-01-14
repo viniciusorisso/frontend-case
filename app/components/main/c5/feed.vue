0a693ae614292d96b198f96eb93c24789d11aa18
viniciusorisso <viniciusorisso@gmail.com>
2024-01-14 13:07:54 -0300

    feat: layout do component de feedback criado

--- app/components/main/c5/feed.vue
+++ app/components/main/c5/feed.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    Feedback
+  <div class="w-full h-full flex flex-col">
+    <div
+      class="flex border rounded-t-xl overflow-hidden"
+      style="border-color: #e2e4e9"
+    >
+      <button
+        type="button"
+        v-for="(emoji, index) in list"
+        class="border-r p-4 w-full flex items-center justify-center"
+        style="border-color: #e2e4e9"
+        :class="{
+          'bg-[#F6F8FA]': index === currentEmoji,
+        }"
+        @click="setEmoji(index)"
+      >
+        {{ emoji }}
+      </button>
+    </div>
+    <textarea
+      type="text"
+      class="p-3 h-full text-sm text-[#868C98] border rounded-b-xl overflow-hidden"
+      style="border-color: #e2e4e9"
+      placeholder="Adicionar comentário"
+    ></textarea>
   </div>
 </template>
+
+<script lang="ts" setup>
+const list = ["😔", "😕", "😐", "🙂", "😄"];
+const currentEmoji = ref<number>();
+
+const setEmoji = (index: number): void => {
+  if (currentEmoji.value === index) {
+    currentEmoji.value = undefined;
+
+    return;
+  }
+  currentEmoji.value = index;
+};
+</script>
--- app/components/main/c5/index.vue
+++ app/components/main/c5/index.vue
@@ -1,27 +1,27 @@
 <template>
-  <section class="bg-white flex flex-col p-4 gap-y-5">
+  <section class="bg-white flex flex-col p-4 gap-y-3 md:gap-y-5">
     <div class="flex gap-x-2">
       <img src="@/assets/svg/feedback.svg" alt="" srcset="" />
       <p class="text-base font-medium">Feedback diário</p>
     </div>
     <CommonDivider />
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center gap-2">
       <img
         class="h-10 w-10"
         src="@/assets/svg/usuario-feedback.svg"
         alt=""
         srcset=""
       />
-      <div>
-        <p class="text-base font-medium">
+      <div class="flex flex-col gap-1 md:gap-3 text-center">
+        <p class="text-sm md:text-base font-medium">
           Como você classificaria seu humor hoje?
         </p>
-        <p class="text-xs font-normal text-[#525866]">
+        <p class="text-[10px] md:text-xs font-normal text-[#525866]">
           Compartilhe seu humor para nos ajudar a entender
         </p>
       </div>
     </div>
-    <MainFeedback />
+    <MainC5Feed />
     <MainC5Button />
   </section>
 </template>
